e369f3104a54d43d060d7ccd6d0a8724f63933f2
Jill <jillannebragado13@gmail.com>
2025-06-15 03:39:04 +0800

    created the sliver, current location and description widget

--- food_delivery_app/lib/pages/home_page.dart
+++ food_delivery_app/lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_current_location.dart';
+import 'package:food_delivery_app/components/my_description_box.dart';
 import 'package:food_delivery_app/components/my_drawer.dart';
+import 'package:food_delivery_app/components/mysliver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,10 +15,36 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
+      
       drawer: MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySliverAppBar(
+            title: Text(
+              'TITLE', 
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Divider(
+                  indent: 25.0,
+                  endIndent: 25.0,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+               //current location
+                MyCurrentLocation(),
+               //description
+               MyDescriptionBox(),
+              ],
+            ),
+            
+          ),
+        ],
+        body: Container(
+          color: Colors.blue,
+        ),
+      ),
     );
   }
 }
