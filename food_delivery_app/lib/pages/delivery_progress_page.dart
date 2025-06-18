8871ff7734a2b5e78402ce8f505867e164d2b52a
Jill <jillannebragado13@gmail.com>
2025-06-19 02:22:21 +0800

    Add receipt generation and display receipt ui

--- food_delivery_app/lib/pages/delivery_progress_page.dart
+++ food_delivery_app/lib/pages/delivery_progress_page.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_receipt.dart';
 
 class DeliveryProgressPage extends StatefulWidget {
   const DeliveryProgressPage({super.key});
@@ -15,23 +16,11 @@ class _DeliveryProgressPageState extends State<DeliveryProgressPage> {
       appBar: AppBar(
         title: const Text("Delivery Progress"),
         backgroundColor: Colors.transparent,
-        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const CircularProgressIndicator(),
-            const SizedBox(height: 20),
-            Text(
-              "Your order is on the way!",
-              style: TextStyle(
-                fontSize: 20,
-                color: Theme.of(context).colorScheme.onSurface,
-              ),
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          MyReceipt(),
+        ],
       ),
     );
   }
